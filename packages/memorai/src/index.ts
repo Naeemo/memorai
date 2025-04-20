ac28c6a94fad36d7c1646f7b3acd5b24c8e31d91
Naeemo <naeemo@qq.com>
2025-04-20 22:21:32 +0800

    WIP Memory

--- packages/memorai/src/index.ts
+++ packages/memorai/src/index.ts
@@ -1,17 +1,51 @@
+interface MemoryPiece {
+  content: string
+  id: string
+  time: Date
+}
+
 export class Memory {
-  public add(): void {
-    console.info('add()')
+  private readonly pieces: MemoryPiece[] = []
+
+  constructor(
+    public readonly user: string,
+    public readonly agent: string,
+  ) {
+    console.info('Memory init', user, agent)
   }
 
-  public update(): void {
-    console.info('Memory.update()')
+  public add(message: string, time: Date = new Date()): MemoryPiece[] {
+    const facts = this.digest(message)
+    const pieces = facts.map((fact) => ({
+      time,
+      content: fact,
+      id: crypto.randomUUID(),
+    }))
+    this.pieces.push(...pieces)
+    return pieces
+  }
+
+  public search(query: string): MemoryPiece[] {
+    console.info('Memory.search()', query)
+    return this.pieces.filter((piece) => {
+      // todo relativity computation
+      console.info('calculate relativity with query', piece, query)
+      return true
+    })
+  }
+
+  public list(pagination?: { page: number; size: number }): MemoryPiece[] {
+    console.info('Memory.list()', pagination)
+    return this.pieces
   }
 
   public remove(): void {
     console.info('Memory.remove()')
   }
 
-  public search(query: string): void {
-    console.info('Memory.search()', query)
+  private digest(message: string): string[] {
+    // todo
+    console.info('Memory digest() for facts', message)
+    return []
   }
 }
